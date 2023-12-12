8b0c1607d8463cd2352b490d14df9c848afe0f31
Rabsickles91 <grewalrabian@gmail.com>
2023-12-11 21:49:56 -0500

    css styling

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
+  let link = <Link to="/Stocks" className= "aboutlink"><div>Stocks.</div></Link> 
+
   return (
-    <div>About</div>
+    <div>
+      <h1 className = "title">Welcome to my super simple Stock app!</h1>
+      <p className= "text">This app was built using a simple API to give the most upto date prices
+        of the list of stocks listed under</p> {link}
+        <br/>
+      <p className= "text">Click on any link above to navigate to a different page.</p>  
+      
+
+
+    </div>
   )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,12 @@
 export default function Home() {
     return(
-        <div>HOMEPAGE</div>
+        <div>
+            <h1>HOMEPAGE</h1>
+            <h2>WELCOME TO ISTOCK</h2>
+            <h2>THE PREMIER LOCATION OF UPTO DATE STOCK PRICES OF YOUR FAVORITE CORPORATIONS!</h2>
+            <img src='https://effusiongallery.com/cdn/shop/products/DSC_0673.jpg?v=1570559261' alt = "$$$$" width="500" height="600"  />
+
+
+        </div>
     )
 }
